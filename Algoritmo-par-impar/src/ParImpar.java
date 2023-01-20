64ca32422e8c3e6564aeab3a99cce93eddfd4e7d
Douglas Nobrega <dougrete2014@outlook.com>
2023-01-20 15:46:24 -0300

    Par e impar

---
+++ Algoritmo-par-impar/src/ParImpar.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ParImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros; 
+
+        System.out.println("Quantidade de números: ");
+        quantNumeros = scan.nextInt();    
+    }
+}
