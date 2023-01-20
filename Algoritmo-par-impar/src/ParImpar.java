2b824aa2d52a307e5a58db4effa46c9c885a7cb3
Douglas Nobrega <dougrete2014@outlook.com>
2023-01-20 15:55:33 -0300

    commit

--- Algoritmo-par-impar/src/ParImpar.java
+++ Algoritmo-par-impar/src/ParImpar.java
@@ -4,9 +4,28 @@ public class ParImpar {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int quantNumeros; 
+        int quantNumeros = 0; 
+        int numero; 
+
+        int quantPares = 0, quantImpares = 0;
 
         System.out.println("Quantidade de números: ");
-        quantNumeros = scan.nextInt();    
+        quantNumeros = scan.nextInt();
+    
+        int count = 0;
+
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            if (numero % 2 == 0) quantPares++;
+            else quantImpares++;
+
+            count++;
+        } while (count < quantNumeros);
+        
+        System.out.println("Quantidade de Par: " + quantPares);
+        System.out.println("Quantidade de Impar: " + quantImpares);
+
     }
 }
