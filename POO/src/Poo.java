0856ce4f665867268cd10e256e2eb9553e750fee
Douglas Nobrega <dougrete2014@outlook.com>
2023-01-25 16:43:24 -0300

    construtor, metodos, get, set, atributos

---
+++ POO/src/Poo.java
@@ -0,0 +1,25 @@
+/*
+ Conceitos básicos 
+Classe, atributo, método, objeto, mensagem
+ */
+
+public class Poo {
+    public static void main(String[] args) {
+        // classe
+        class Carro {
+            // atributo
+            int portas;
+
+            // exemplo de método
+            void frear() {
+            }
+
+            // exemplo de construtor - cria objetos a parti dessa classe
+            Carro() {
+            }
+
+            // exemplo de destrutor - coletor de lixo dessa classe
+            // void finalize() {}
+        }
+    }
+}
