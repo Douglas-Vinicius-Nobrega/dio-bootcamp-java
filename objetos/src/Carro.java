cf91916b003dee8e99a8555ef01247ff466d119a
Douglas Nobrega <dougrete2014@outlook.com>
2023-01-26 16:13:57 -0300

    Objeto , mensagem e exemplos

---
+++ objetos/src/Carro.java
@@ -0,0 +1,51 @@
+class Carro {
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    // construtor
+    Carro() {
+
+    }
+
+    // construtor com sobrecarga
+    Carro(String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    // set = colocar um valor em um atributo, colocar um valor no atributo cor.
+    void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    // get = retorne a cor desse carro. Se vai retorna algo, não pode ser void.
+    String getCor() {
+        return cor;
+    }
+
+    void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    String getModelo() {
+        return modelo;
+    }
+
+    // get e o set do tanque
+    void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    // método de total para encher o tanque - empplementar uma lógica aqui
+
+    double totalValorTanque(double valorCombustivel) {
+        return capacidadeTanque * valorCombustivel;
+    }
+
+}
---
+++ objetos/src/Exemplos.java
@@ -0,0 +1,11 @@
+public class Exemplos {
+    public static void main(String[] args) {
+        // objeto criação
+        // construir um objeto Carro e adicionar na variavel carro
+        // Carro classe - carro objeto
+        Exemplos carro = new Exemplos();
+        // chamando um métodos do objeto carro.<métodos>;
+
+        // chamando um métodos do objeto via classe Carro.<métodos>;
+    }
+}
